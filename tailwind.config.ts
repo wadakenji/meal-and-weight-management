ed21632a94140f499c925c3a2e9921271bbcc97d
wadakenji <ponporopen@gmail.com>
2024-12-28 05:39:21 +0900

    chore: tailwind.config更新

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,27 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+    colors: {
+      text: {
+        default: '#212121',
       },
+    },
+    fontSize: {
+      base: '16px',
+    },
+    spacing: {
+      '1px': '1px',
+      '2px': '2px',
+      '4px': '4px',
+      '8px': '8px',
+      '16px': '16px',
+      '24px': '24px',
+      '32px': '32px',
     },
   },
   plugins: [],
