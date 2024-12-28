bb64c9aa960dbaff393dc322bd5425297a5bab72
wadakenji <ponporopen@gmail.com>
2024-12-28 19:50:02 +0900

    chore: supabaseのclientパッケージ導入

---
+++ src/libs/supabase/createClient.ts
@@ -0,0 +1,23 @@
+import { createServerClient } from '@supabase/ssr';
+import { cookies } from 'next/headers';
+import { ENV } from '@/constants/env';
+
+export const createSupabaseServerClient = async () => {
+  const cookieStore = await cookies();
+  return createServerClient(ENV.SUPABASE_URL, ENV.SUPABASE_ANON_KEY, {
+    cookies: {
+      getAll() {
+        return cookieStore.getAll();
+      },
+      setAll(cookiesToSet) {
+        try {
+          cookiesToSet.forEach(({ name, value, options }) =>
+            cookieStore.set(name, value, options),
+          );
+        } catch {
+          // Cookies can only be modified in a Server Action or Route Handler.
+        }
+      },
+    },
+  });
+};
