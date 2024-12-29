153a1fa709ab3548d538c8f1262f2735e0875975
wadakenji <ponporopen@gmail.com>
2024-12-29 09:53:22 +0900

    refactor: type error を一時的に解消

--- src/app/actions/sign-in.ts
+++ src/app/actions/sign-in.ts
@@ -7,6 +7,10 @@ export const signInAction = async (formData: FormData) => {
   const email = formData.get('email');
   const password = formData.get('password');
 
+  // todo validation
+  if (typeof email !== 'string' || typeof password !== 'string')
+    throw new Error('');
+
   await signIn(email, password).catch((e) => {
     console.error(e);
     throw new Error('');
