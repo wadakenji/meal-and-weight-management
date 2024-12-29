3bf61dd3150df4b86e2a701ef89c50eb6119477f
wadakenji <ponporopen@gmail.com>
2024-12-29 20:31:46 +0900

    chore: supabase db関係の型定義生成

--- src/libs/supabase/createClient.ts
+++ src/libs/supabase/createClient.ts
@@ -1,10 +1,11 @@
 import { createServerClient } from '@supabase/ssr';
 import { cookies } from 'next/headers';
 import { ENV } from '@/constants/env';
+import { Database } from '@/types/supabase';
 
 export const createSupabaseServerClient = async () => {
   const cookieStore = await cookies();
-  return createServerClient(ENV.SUPABASE_URL, ENV.SUPABASE_ANON_KEY, {
+  return createServerClient<Database>(ENV.SUPABASE_URL, ENV.SUPABASE_ANON_KEY, {
     cookies: {
       getAll() {
         return cookieStore.getAll();
