d4eb2fc3911b0282596ce11be1d778c34c1af8db
wadakenji <ponporopen@gmail.com>
2025-01-01 20:05:24 +0900

    feat: middlewareを導入し、認証状態によるリダイレクトを実装

---
+++ src/middleware.ts
@@ -0,0 +1,34 @@
+import { getUser } from '@/usecase/user';
+import { NextRequest, NextResponse } from 'next/server';
+
+export const middleware = async (request: NextRequest) => {
+  const user = await getUser();
+  const isLoggedIn = !!user;
+
+  switch (request.nextUrl.pathname) {
+    case '/':
+      const redirectPath = isLoggedIn ? '/register' : '/sign-in';
+      return NextResponse.redirect(new URL(redirectPath, request.url));
+    case '/sign-in':
+      if (isLoggedIn)
+        return NextResponse.redirect(new URL('/register', request.url));
+      break;
+    default:
+      if (!isLoggedIn)
+        return NextResponse.redirect(new URL('/sign-in', request.url));
+      break;
+  }
+};
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico, sitemap.xml, robots.txt (metadata files)
+     */
+    '/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)',
+  ],
+};
