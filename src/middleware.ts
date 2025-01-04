5e2f3b05e2817a796ecfaf2706b5d1da7ae78200
wadakenji <ponporopen@gmail.com>
2025-01-04 15:43:10 +0900

    docs: コメント修正

--- src/middleware.ts
+++ src/middleware.ts
@@ -27,7 +27,7 @@ export const config = {
      * - api (API routes)
      * - _next/static (static files)
      * - _next/image (image optimization files)
-     * - favicon.ico, sitemap.xml, robots.txt (metadata files)
+     * - favicon.ico, sitemap.xml, robots.txt, manifest.webmanifest (metadata files)
      */
     '/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt|manifest.webmanifest).*)',
   ],
