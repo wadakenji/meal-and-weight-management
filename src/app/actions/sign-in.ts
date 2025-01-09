518ba7b790cdce2aa6a01771ba8a07b7bf8da09c
wadakenji <ponporopen@gmail.com>
2025-01-09 17:33:44 +0900

    feat: ログインserver action のエラーハンドリング

--- src/app/actions/sign-in.ts
+++ src/app/actions/sign-in.ts
@@ -3,18 +3,18 @@
 import { signIn } from '@/usecase/authentication';
 import { redirect } from 'next/navigation';
 import { validateAndParseSignInFormData } from '@/helpers/form/sign-in';
+import { ERROR_MESSAGES } from '@/constants/error-message';
 
 export const signInAction = async (formData: FormData) => {
   const parseResult = validateAndParseSignInFormData(formData);
 
-  if (!parseResult) return; // todo error handling
+  if (!parseResult)
+    redirect(`/sign-in?error=${encodeURI(ERROR_MESSAGES.SIGN_IN_FAILED)}`);
 
   const { email, password } = parseResult;
   await signIn(email, password).catch((e) => {
     console.error(e);
-    const errorMessage = 'ログインに失敗しました。再度やり直してください。';
-    redirect(`/sign-in?error=${encodeURI(errorMessage)}`);
-    // todo error handling
+    redirect(`/sign-in?error=${encodeURI(ERROR_MESSAGES.SIGN_IN_FAILED)}`);
   });
 
   redirect('/');
--- src/constants/error-message.ts
+++ src/constants/error-message.ts
@@ -1,5 +1,6 @@
 export const ERROR_MESSAGES = {
   INTERNAL_SERVER_ERROR: '予期せぬエラーが発生しました。',
+  SIGN_IN_FAILED: 'ログインに失敗しました。再度やり直してください。',
   NOT_AUTHORIZED: 'ログインし直してください。',
   INVALID_USER_INPUT:
     '入力内容が正しくありません。もう一度やり直してください。',
