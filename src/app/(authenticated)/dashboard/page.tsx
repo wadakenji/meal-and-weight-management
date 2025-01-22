67d001a54dfd1cd60f3e53d3880b049cdb0e6f1b
wadakenji <ponporopen@gmail.com>
2025-01-22 17:08:50 +0900

    feat: ホーム画面の食事登録時にcacheをinvalidateすることで画面表示を更新する

--- src/app/(authenticated)/dashboard/page.tsx
+++ src/app/(authenticated)/dashboard/page.tsx
@@ -1,22 +1,17 @@
 import { FC } from 'react';
 import { AuthenticatedTemplate } from '../_components/template/authenticated-template/authenticated-template';
-import { getTodayTotalEnergy } from '@/usecase/meal';
 import { TodayWeightPanel } from '@/app/(authenticated)/dashboard/_components/weight/today-weight-panel';
 import { YesterdayStepPanel } from '@/app/(authenticated)/dashboard/_components/step/yesterday-step-panel';
 import { TodayTotalEnergyPanel } from '@/app/(authenticated)/dashboard/_components/meal/today-total-energy-panel';
 
 const Page: FC = async () => {
-  const todayTotalEnergyPromise = getTodayTotalEnergy();
-
   return (
     <AuthenticatedTemplate pageTitle="ホーム">
       <div className="mb-8px flex items-baseline gap-x-8px">
         <YesterdayStepPanel />
         <TodayWeightPanel />
       </div>
-      <TodayTotalEnergyPanel
-        todayTotalEnergyPromise={todayTotalEnergyPromise}
-      />
+      <TodayTotalEnergyPanel />
     </AuthenticatedTemplate>
   );
 };
