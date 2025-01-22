aa79719eb28e29cb8df16a7cf4e18aa20af9c6d2
wadakenji <ponporopen@gmail.com>
2025-01-22 17:01:18 +0900

    feat: ホーム画面の体重登録時にcacheをinvalidateすることで画面表示を更新する

--- src/app/(authenticated)/dashboard/page.tsx
+++ src/app/(authenticated)/dashboard/page.tsx
@@ -1,20 +1,18 @@
 import { FC } from 'react';
 import { AuthenticatedTemplate } from '../_components/template/authenticated-template/authenticated-template';
-import { getTodayWeight } from '@/usecase/weight-record';
 import { getTodayTotalEnergy } from '@/usecase/meal';
 import { TodayWeightPanel } from '@/app/(authenticated)/dashboard/_components/weight/today-weight-panel';
 import { YesterdayStepPanel } from '@/app/(authenticated)/dashboard/_components/step/yesterday-step-panel';
 import { TodayTotalEnergyPanel } from '@/app/(authenticated)/dashboard/_components/meal/today-total-energy-panel';
 
 const Page: FC = async () => {
-  const todayWeightPromise = getTodayWeight();
   const todayTotalEnergyPromise = getTodayTotalEnergy();
 
   return (
     <AuthenticatedTemplate pageTitle="ホーム">
       <div className="mb-8px flex items-baseline gap-x-8px">
         <YesterdayStepPanel />
-        <TodayWeightPanel todayWeightPromise={todayWeightPromise} />
+        <TodayWeightPanel />
       </div>
       <TodayTotalEnergyPanel
         todayTotalEnergyPromise={todayTotalEnergyPromise}
