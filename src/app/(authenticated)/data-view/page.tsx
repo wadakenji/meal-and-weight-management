ff480805aeece8fe8464a041c843da3566cd6056
wadakenji <ponporopen@gmail.com>
2025-01-25 22:47:40 +0900

    feat: データ表示画面でDataViewコンポーネントを使用

--- src/app/(authenticated)/data-view/page.tsx
+++ src/app/(authenticated)/data-view/page.tsx
@@ -1,28 +1,17 @@
 import { FC, Suspense } from 'react';
 import { AuthenticatedTemplate } from '@/app/(authenticated)/_components/template/authenticated-template/authenticated-template';
-import { getLastOneMonthWeightRecords } from '@/usecase/weight-record';
 import { IconSpinner } from '@/components/icon/spinner';
-import { getBelongingUserGroups } from '@/usecase/user-group';
-import { UserDataList } from '@/app/(authenticated)/data-view/_components/user-data-list/user-data-list';
+import { getRelatedUsers } from '@/usecase/user-group';
+import { DataView } from '@/app/(authenticated)/data-view/_components/data-view/data-view';
 
-// todo データの見せ方検討
 const Page: FC = async () => {
-  const userDataPromise = getBelongingUserGroups().then((userGroups) => {
-    const users = userGroups[0].users;
-    return Promise.all(
-      users.map(async ({ id, name }) => ({
-        userId: id,
-        username: name,
-        weightRecords: await getLastOneMonthWeightRecords(id),
-      })),
-    );
-  });
+  const usersPromise = getRelatedUsers();
 
   return (
-      <Suspense fallback={<IconSpinner mxAuto />}>
-        <UserDataList userDataPromise={userDataPromise} />
-      </Suspense>
     <AuthenticatedTemplate pageTitle="データ表示">
+      <Suspense fallback={<IconSpinner mxAuto />}>
+        <DataView usersPromise={usersPromise} />
+      </Suspense>
     </AuthenticatedTemplate>
   );
 };
