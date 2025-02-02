faece594eaaf7dbc535821a169bf5b281fa1d8dc
wadakenji <ponporopen@gmail.com>
2025-02-03 01:52:53 +0900

    feat: 通知クリック時の動作を修正

--- public/sw.js
+++ public/sw.js
@@ -15,5 +15,5 @@ self.addEventListener('push', function (event) {
 self.addEventListener('notificationclick', function (event) {
   console.log('Notification click received.');
   event.notification.close();
-  event.waitUntil(clients.openWindow('<https://your-website.com>'));
+  event.waitUntil(clients.openWindow(process.env.NEXT_PUBLIC_BASE_URL));
 });
