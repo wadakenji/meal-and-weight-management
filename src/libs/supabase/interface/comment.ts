3beb116c557c291c7d98e93f14af7675f8b78251
wadakenji <ponporopen@gmail.com>
2025-04-03 13:58:00 +0900

    feat: comment関連のusecase関数追加

---
+++ src/libs/supabase/interface/comment.ts
@@ -0,0 +1,27 @@
+import { Database } from '@/types/supabase';
+import { datetimeColumnValueToDate } from '@/utils/date';
+
+type CommentRow = Database['public']['Tables']['comments']['Row'];
+type CommentProps = Database['public']['Tables']['comments']['Insert'];
+
+export const commentRowToComment = (commentRow: CommentRow): CommentType => {
+  return {
+    id: commentRow.id,
+    receiverId: commentRow.receiver_id,
+    senderId: commentRow.sender_id,
+    date: commentRow.date,
+    comment: commentRow.comment,
+    createdAt: datetimeColumnValueToDate(commentRow.created_at),
+  };
+};
+
+export const commentToCommentProps = (
+  comment: CommentToCreate,
+): CommentProps => {
+  return {
+    receiver_id: comment.receiverId,
+    sender_id: comment.senderId,
+    date: comment.date,
+    comment: comment.comment,
+  };
+};
---
+++ src/usecase/comments/create-comment.ts
@@ -0,0 +1,39 @@
+import { createSupabaseServerClient } from '@/libs/supabase/createClient';
+import { UsecaseAuthError, UsecaseDbError } from '@/usecase/shared/error';
+import {
+  commentRowToComment,
+  commentToCommentProps,
+} from '@/libs/supabase/interface/comment';
+
+export const createComment = async (
+  comment: Omit<CommentToCreate, 'senderId'>,
+) => {
+  const supabaseClient = await createSupabaseServerClient();
+
+  const authResponse = await supabaseClient.auth.getUser();
+  if (authResponse.error) {
+    console.error(authResponse.error);
+    throw new UsecaseAuthError({
+      module: 'comment',
+      function: 'createComment',
+    });
+  }
+
+  const authUser = authResponse.data.user;
+
+  const res = await supabaseClient
+    .from('comments')
+    .insert(commentToCommentProps({ ...comment, senderId: authUser.id }))
+    .select()
+    .single();
+
+  if (res.error) {
+    console.error(res.error);
+    throw new UsecaseDbError({
+      module: 'comment',
+      function: 'createComment',
+    });
+  }
+
+  return commentRowToComment(res.data);
+};
