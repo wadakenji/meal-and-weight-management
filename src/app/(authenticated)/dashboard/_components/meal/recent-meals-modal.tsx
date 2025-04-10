e8eb36932805f8d31ce323822b49754df612456f
wadakenji <ponporopen@gmail.com>
2025-04-10 14:46:55 +0900

    feat: 最近の食事モーダルのスタイルを変更

--- src/app/(authenticated)/dashboard/_components/meal/recent-meals-modal.tsx
+++ src/app/(authenticated)/dashboard/_components/meal/recent-meals-modal.tsx
@@ -21,16 +21,24 @@ export const RecentMealsModal: FC<Props> = ({
       <h2 className="mb-24px text-center text-xl font-bold">
         最近の食べたもの
       </h2>
-      <ul>
+      <ul className="flex flex-wrap gap-8px *:basis-[calc((100%-8px)/2)]">
         {recentMeals.map((meal) => (
-          <li key={meal.id}>
+          <li
+            key={meal.id}
+            className="min-h-min-button-size rounded border border-line"
+          >
             <button
               onClick={() => {
                 onClickMeal(meal);
                 close();
               }}
+              className="flex size-full flex-col items-center justify-between p-16px"
             >
-              {meal.name}
+              <span className="mb-8px text-lg font-bold">{meal.name}</span>
+              <span className="text-text-gray">
+                <span className="">{meal.amountOfEnergy}</span>
+                <span className="text-sm"> kcal</span>
+              </span>
             </button>
           </li>
         ))}
